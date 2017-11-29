fcec982f2c07be89135a84f7025074d9866f2331
Corithian Williams <corithianmw96@gmail.com>
2017-11-28 20:20:02 -0500

    Added testing code

--- test/geometric/smash/property/PropertyTest.java
+++ test/geometric/smash/property/PropertyTest.java
@@ -5,6 +5,8 @@
  */
 package geometric.smash.property;
 
+import geometric.smash.DoubleModifier;
+import geometric.smash.IntegerModifier;
 import geometric.smash.Property;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -16,14 +18,14 @@ import static org.junit.Assert.*;
  * @author AceForce
  */
 public class PropertyTest {
-    
+
     public PropertyTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
@@ -31,12 +33,38 @@ public class PropertyTest {
     @Test
     public void testGetValue() {
         System.out.println("getValue");
+        Property<Double> instance = new Property<>(5.239);
+        instance.addModifier(3, new DoubleModifier.Adder(2.0));
+        instance.addModifier(0, new DoubleModifier.Multiplier(2.32));
+        instance.addModifier(2, new DoubleModifier.Divider(2.23));
+        instance.addModifier(1, new DoubleModifier.Adder(8.0));
+        double expResult = (((5.239 * 2.32) + 8.0) / 2.23) + 2.0;
+        double result = instance.getValue();
+        
+        assertEquals(expResult, result, 0.001);
+        System.out.format("expected: %f\n\tactual: %f\n", expResult, result);
+
+    }
+
+
+    @Test
+    public void test2GetValue() {
+        System.out.println("getValue");
         Property<Integer> instance = new Property<>(5);
+        instance.addModifier(2, new IntegerModifier.Divider(3));
+        instance.addModifier(0, new IntegerModifier.Multiplier(2));
+        instance.addModifier(3, new IntegerModifier.Adder(2));
+        instance.addModifier(1, new IntegerModifier.Adder(8));
+        
         int expResult = (((5 * 2) + 8) / 3) + 2;
         int result = instance.getValue();
+        
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.format("expected: %d\n\tactual: %d\n", expResult, result);
+
     }
-    
+
+    public static void main(String args[]) {
+        org.junit.runner.JUnitCore.main("geometric.smash.property.PropertyTest");
+    }
 }
